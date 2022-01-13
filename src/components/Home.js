e9fc640518faddd5bec90863496c7b8f6d442e42
Tushar <tusharvamdoskar#gmail.com>
2022-01-13 18:27:04 +0530

    3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,30 @@
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 function Home() {
   return (
     <div className="Home">
         <Carousel showArrows={true} swipeable={true} >
             <div>
+            <TransformWrapper>
+                <TransformComponent>
                 <img src="https://i.ibb.co/xmMZHB1/Matsya-Menu-AC-F-page-001.jpg" />
+                </TransformComponent>
+            </TransformWrapper>
             </div>
             <div>
+            <TransformWrapper>
+                <TransformComponent>
                 <img src="https://i.ibb.co/9WhCZfh/Matsya-Menu-AC-F-page-002.jpg" />
+                </TransformComponent>
+            </TransformWrapper>
             </div>
             <div>
+            <TransformWrapper>
+                <TransformComponent>
                 <img src="https://i.ibb.co/xSkrQKx/Matsya-Menu-AC-F-page-003.jpg" />
+                </TransformComponent>
+            </TransformWrapper>
             </div>
             <div>
                 <img src="https://i.ibb.co/Mh4jvhk/Matsya-Menu-AC-F-page-004.jpg" />
